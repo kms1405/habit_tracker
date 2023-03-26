80d2ee0883d17bf836df91888e7b8b95cbf9ae99
User <meenakshisundaram.k@tendercuts.in>
2023-03-26 12:10:24 +0530

    final changes

--- controllers/showHistory.js
+++ controllers/showHistory.js
@@ -3,7 +3,7 @@ const Task = require("../models/tasks");
 module.exports.showHistoryController = async function (req, res) {
     date = req.query.date
     if (!date) {
-        var date = new Date(); 
+        var date = new Date();
     } else {
         date = new Date(date);
     }
@@ -11,20 +11,19 @@ module.exports.showHistoryController = async function (req, res) {
     end = new Date(date)
     date.setDate(date.getDate() - 7);
 
-    // console.log("end date",end, "start date",date)
 
     const lastSevenDayReport = await Task.find({
         createdAt: {
             $gte: new Date(date.setHours(00, 00, 00)),
             $lt: new Date(end.setHours(23, 59, 59)),
         },
-    }).sort({createdAt:"desc"});
+    }).sort({ createdAt: "desc" });
 
     function groupBy(list) {
         const map = new Map();
         list.forEach((item) => {
-            date_element=item.createdAt.toString().split(" ")
-            key = date_element[0] + " " + date_element[1] + " " + date_element[2] + " " +date_element[3]
+            date_element = item.createdAt.toString().split(" ")
+            key = date_element[0] + " " + date_element[1] + " " + date_element[2] + " " + date_element[3]
             const collection = map.get(key)
             if (!collection) {
                 map.set(key, [item]);
